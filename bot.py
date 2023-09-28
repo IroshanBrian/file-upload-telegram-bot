c6273fb5871d78a3016302e879c69c4b3ff5b083
IroshanBrian <brianstudiolk@gmail.com>
2023-09-28 23:38:24 +0530

    #

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ from typing import final
 from telegram import Update
 from telegram.ext import Applicationm, CommandHandler, MessageHandler, filters, ContextTypes
 
+TOKEN: final = ''
+BOT_USERNAME: final = ''
+
 
 # commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -15,7 +18,7 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
      
 # responses
 
-def handle_responses(text: str)-> str:
+def handle_response(text: str)-> str:
      processed: str = text.lower()
      
      if 'hello' in text:
@@ -25,3 +28,44 @@ def handle_responses(text: str)-> str:
      if 'hey' in text:
           return 'Hey There!'
      return 'I can`t understand'
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+     message_type: str = update.message.chat.type
+     text: str = update.message.text
+     
+     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+     
+     if message_type == 'group':
+          if BOT_USERNAME in text:
+               new_text: str = text.replace(BOT_USERNAME, '').strip()
+               response: str = handle_response(new_text)
+          else:
+               response: str = handle_response(text)
+          
+          print('Bot:', response)
+          await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+     print(f'Update {update} caused error {context.error}')
+     
+if __name__ == '__main__':
+     app = Application.builder().token(TOKEN).build()
+     
+     # commands
+     add.add_handler(CommandHandler('start', start_command))
+     add.add_handler(CommandHandler('help', start_command))
+     add.add_handler(CommandHandler('custom', start_command))
+     
+     # messages
+     app.add_handler(MessageHandler(filter.TEXT, handle_message))
+     
+     
+     # errors
+     app.add_error_handler(error)
+     
+     # polls the bot
+     print('Polling...')
+     app.run_polling(poll_intervel=3)
+          
+               
