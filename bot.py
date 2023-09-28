b2836bc98caea4d17da55bf4a74d060f11dba1c8
IroshanBrian <brianstudiolk@gmail.com>
2023-09-28 23:00:05 +0530

    #

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from typing import final
 from telegram import Update
 from telegram.ext import Applicationm, CommandHandler, MessageHandler, filters, ContextTypes
 
+
+# commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
      await update.message.reply_text('Hello! Yuta is at ur service 😀. How can i help you?')
      
@@ -10,3 +12,16 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
      
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
      await update.message.reply_text('This is custom command')
+     
+# responses
+
+def handle_responses(text: str)-> str:
+     processed: str = text.lower()
+     
+     if 'hello' in text:
+          return 'Hey There!'
+     if 'hi' in text:
+          return 'Hey There!'
+     if 'hey' in text:
+          return 'Hey There!'
+     return 'I can`t understand'
